45b1cd4b92e85a97ce2a57c6d2202d66ae6c0a36
AkshaySonawane86 <akshays181999@gmail.com>
2025-01-27 17:20:43 +0530

    new change

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Login from './componets/Login';
+import AddEmp from './componets/AddEmp';
+
+function App() {
+  return (
+   <>
+    <Login />
+    <AddEmp />
+   </>
+  );
+}
+
+export default App;
